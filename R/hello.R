38b340516a8ad01e0e832e42a86bc6b1f3a9d6a7
Allen Tang <allentang.me@gmail.com>
2017-02-14 09:40:10 -0800

    add package framework

---
+++ R/hello.R
@@ -0,0 +1,25 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
+
+#' An S4 class to represent a bank account.
+#'
+#' @slot balance A length-one numeric vector
+Account <- setClass("Account",
+                    slots = list(balance = "numeric")
+)
