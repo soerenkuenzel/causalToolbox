166506dfdb8e99c88a87122afaf7c87410dbb6c3
soerenkuenzel <srk@berkeley.edu>
2017-03-10 09:31:12 -0800

    Updating Xhrf test

--- tests/testcases/Xhrf.R
+++ tests/testcases/Xhrf.R
@@ -1,11 +1,13 @@
 devtools::load_all()
 library(hte)
-# Use Iris dataset
+# Example 1: Use Iris dataset
 set.seed(1423614230)
 
 feat <- iris[, -1]
 tr <- rbinom(nrow(iris), 1, .5)
 yobs <- iris[, 1]
+
+?X_RF
 
 xl <- X_RF(
   feat = feat,
@@ -14,3 +16,22 @@ xl <- X_RF(
 )
 
 EstimateCate(xl, feat)
+
+
+# Example 2: large n
+set.seed(400189)
+n <- 10000
+d <- 12
+
+feat <- as.data.frame(matrix(rnorm(n*d), nrow = n))
+tr <- rbinom(n, 1, feat$V1/ 20 +.5)
+yobs <- feat$V1  + feat$V2 + tr * feat$V3
+
+# this takes approximately 2 min to run.
+xl <- X_RF(
+  feat = feat,
+  tr = tr,
+  yobs = yobs
+)
+
+
