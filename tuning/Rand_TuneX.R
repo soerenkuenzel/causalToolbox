a9743b67c82fa2cc706a3da94899e1224cdd793d
soerenkuenzel <srk@berkeley.edu>
2017-03-11 16:37:56 -0800

    Decided to add TuneX
    I think it is better to have tuning file in the GitHub folder.

---
+++ tuning/Rand_TuneX.R
@@ -0,0 +1,86 @@
+# In this file we tune X such that it
+
+
+library(hte)
+
+nsamples <- 10000
+dim <- 12
+ntrain <- 1000
+ntest <- 10000
+alpha <- .1
+setup <- "RsparseT2weak"
+nthread <- 32
+
+# Sample each parameter
+Rand_tune <- data.frame(
+  dim = dim,
+  ntrain = ntrain,
+  mtry_first = sample(1:dim, nsamples, replace = TRUE),
+  mtry_second = sample(1:dim, nsamples, replace = TRUE),
+  min_node_size_spl_first = sample(c(1, 3, 10, 30, 100), nsamples, replace = TRUE),
+  min_node_size_spl_second = sample(c(1, 3, 10, 30, 100), nsamples, replace = TRUE),
+  min_node_size_ave_first = sample(c(1, 3, 10, 30, 100), nsamples, replace = TRUE),
+  min_node_size_ave_second = sample(c(1, 3, 10, 30, 100), nsamples, replace = TRUE),
+  splitratio_first = sample(c(.01, .05, .1, .2, .3, .5, .8), nsamples, replace = TRUE),
+  splitratio_second = sample(c(.01, .05, .1, .2, .3, .5, .8), nsamples, replace = TRUE),
+  replace_first = sample(c(TRUE, FALSE), nsamples, replace = TRUE),
+  replace_second = sample(c(TRUE, FALSE), nsamples, replace = TRUE),
+  sample_fraction_first = sample(c(.01, .1, .3, .5, 0.632, .7, .9, 1), nsamples,
+                                 replace = TRUE),
+  sample_fraction_second = sample(c(.01, .1, .3, .5, 0.632, .7, .9, 1), nsamples,
+                                  replace = TRUE)
+)
+
+if (!dir.exists("Tune/XLearner/"))
+  dir.create("Tune/XLearner/")
+
+# loop through rows and fill in the NA columns:
+for (i in 1:nsamples) {
+  print(paste("Starting with", i, "of", nsamples))
+  stat.time <- Sys.time()
+  # run the experiment:
+  experiment <- simulate_causal_experiment(
+    ntrain = ntrain,
+    ntest = ntest,
+    dim = dim,
+    alpha = alpha,
+    setup = setup
+  )
+
+  # train the X-learner:
+  L <- X_RF(
+    feat = experiment$feat_tr,
+    tr = experiment$W_tr,
+    yobs = experiment$Yobs_tr,
+    predmode = "propmean",
+    num_trees_first = 500,
+    num_trees_second = 500,
+    mtry_first = Rand_tune$mtry_first[i],
+    mtry_second = Rand_tune$mtry_second[i],
+    min_node_size_spl_first = Rand_tune$min_node_size_spl_first[i],
+    min_node_size_spl_second = Rand_tune$min_node_size_spl_second[i],
+    min_node_size_ave_first = Rand_tune$min_node_size_ave_first[i],
+    min_node_size_ave_second = Rand_tune$min_node_size_ave_second[i],
+    splitratio_first = Rand_tune$splitratio_first[i],
+    splitratio_second = Rand_tune$splitratio_second[i],
+    replace_first = Rand_tune$replace_first[i],
+    replace_second = Rand_tune$replace_second[i],
+    sample_fraction_first = Rand_tune$sample_fraction_first[i],
+    sample_fraction_second = Rand_tune$sample_fraction_second[i],
+    nthread = nthread
+  )
+  EMSE <- mean((tau_te - EstimateCate(L, feat_te)) ^ 2)
+
+  Rand_tune[i, setup] <- EMSE
+  print(paste(
+    "   Done with",
+    setup,
+    "Error was:",
+    EMSE,
+    "   and it took",
+    as.numeric(Sys.time() - stat.time, units = "mins"),
+    " minutes."
+  ))
+  write.csv(Rand_tune,
+            paste0("Tune/XLearner/EvalD", dim, "N", ntrain, ".csv"))
+}
