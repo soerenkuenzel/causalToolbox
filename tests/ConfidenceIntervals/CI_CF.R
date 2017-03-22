f09b51c49d9b53162308850624f665a3c7a7efb4
soerenkuenzel <srk@berkeley.edu>
2017-03-22 03:06:33 -0700

    Setting up CF tests

---
+++ tests/ConfidenceIntervals/CI_CF.R
@@ -0,0 +1,71 @@
+# In this document, we test the performance of confidence intervals using
+# X_hRF.
+
+library(hte)
+library(causalForest)
+library(randomForestCI)
+library(causalForest)
+seed <- 2
+set.seed(seed)
+
+setup_loop <-
+  c("RsparseT2weak", "RespSparseTau1strong", "complexTau")
+dim_loop <- c(20, 50, 100)
+ntrain_loop <- c(100, 500, 1000, 2000, 10000, 100000)
+
+# Experiment 1:
+for (ntrain in ntrain_loop) {
+  for (setup in setup_loop) {
+    for (dim in dim_loop) {
+
+      print(paste("setup =", setup, "dim =", dim, "ntrain =", ntrain))
+
+      experiment <- simulate_causal_experiment(
+        ntrain = ntrain,
+        ntest = 1000,
+        dim = dim,
+        alpha = .1,
+        setup = setup
+      )
+
+      forest <-
+        causalForest(
+          X = experiment$feat_tr,
+          Y = experiment$Yobs_tr,
+          W = experiment$W_tr,
+          num.trees = 100,
+          sample.size = round(length(experiment$Yobs_tr) / 2)
+        )
+      #make predictions on the test sample
+      predictions = predict(forest, experiment$feat_te)
+
+      CIs <- tryCatch({
+        #get confidence intervals on the predictions
+        forest.ci = randomForestInfJack(forest,
+                                        experiment$feat_te,
+                                        calibrate = TRUE)
+        cbind(predictions, forest.ci)
+      },
+      error = function(e) {
+        print(e)
+        forest.ci = randomForestInfJack(forest,
+                                        experiment$feat_te,
+                                        calibrate = FALSE)
+        cbind(predictions, forest.ci)
+      })
+
+      write.csv(
+        cbind(CIs, experiment$tau_te),
+        file = paste0(
+          "data/causalForest__",
+          setup,
+          "_n",
+          ntrain,
+          "_dim",
+          dim,
+          "_alpha.1_B100.csv"
+        )
+      )
+    }
+  }
+}
