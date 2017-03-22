dccc8ab9d8a78dc93c74bd4c4d306983f67dea78
soerenkuenzel <srk@berkeley.edu>
2017-03-21 17:12:07 -0700

    Setting up the confidence interval simulations

---
+++ tests/ConfidenceIntervals/CI_X_hRF.R
@@ -0,0 +1,47 @@
+# In this document, we test the performance of confidence intervals using
+# X_hRF.
+
+
+library(hte)
+seed <- 2
+set.seed(seed)
+
+setup_loop <- c("RsparseT2weak", "RespSparseTau1strong", "complexTau")
+dim_loop <- c(20, 50, 100)
+
+# Experiment 1:
+for (setup in setup_loop) {
+  for (dim in dim_loop) {
+    experiment <- simulate_causal_experiment(
+      ntrain = 1000,
+      ntest = 1000,
+      dim = dim,
+      alpha = .1,
+      setup = "RsparseT2weak"
+    )
+
+    L <- X_RF(
+      feat = experiment$feat_tr,
+      tr = experiment$W_tr,
+      yobs = experiment$Yobs_tr,
+      predmode = "propmean",
+      ntree_first = 100,
+      ntree = 100,
+      nthread = 8
+    )
+
+    mean((experiment$tau_te - EstimateCate(L, experiment$feat_te)) ^ 2)
+    CIs <- CateCI(L, feature_new = experiment$feat_te, B = 100)
+
+    write.csv(
+      cbind(CIs, experiment$tau_te),
+      file = paste0(
+        "tests/ConfidenceIntervals/data/",
+        setup,
+        "_n1000_dim",
+        dim,
+        "_alpha.1_B100.csv"
+      )
+    )
+  }
+}
