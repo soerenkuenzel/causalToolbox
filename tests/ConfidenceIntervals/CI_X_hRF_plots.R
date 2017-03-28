f9f86df7cacc7954d4b17471104e4d8c97f1fe7c
soerenkuenzel <srk@berkeley.edu>
2017-03-28 00:50:12 -0700

    Creating software for summary plots of CI performance of XhRF

--- tests/ConfidenceIntervals/CI_X_hRF_plots.R
+++ tests/ConfidenceIntervals/CI_X_hRF_plots.R
@@ -3,6 +3,10 @@
 library(ggplot2)
 library(dplyr)
 
+
+#################################
+### Read in the data ############
+#################################
 files <- dir("tests/ConfidenceIntervals/data/")
 
 CI <- data.frame()
@@ -52,6 +56,10 @@ n_loop <- unique(CI$n)
 dim_loop <- unique(CI$dim)
 
 
+########################################
+###### Single plots ####################
+########################################
+
 for (setup_i in setup_loop) {
   for (n_i in n_loop) {
     for (dim_i in dim_loop) {
@@ -84,16 +92,14 @@ for (setup_i in setup_loop) {
           ylim = c(lower_boundary, upper_boundary)
         ) +
         theme_bw() +
-        ylab("predicted CATE") +
+        ylab("estimated CATE") +
         xlab("true CATE") +
         ggtitle(paste0(
-          setup_i,
-          ", dim  =",
+          "dim  = ",
           dim_i,
-          ", n =",
+          ", n = ",
           n_i,
-          ", alpha = .1",
-          ", coverage =",
+          ", coverage = ",
           coverage
         ))
 
@@ -106,8 +112,57 @@ for (setup_i in setup_loop) {
           "_n",
           n_i,
           ".pdf"
-        )
+        ),
+        width = 11,
+        height = 7
       )
+      print("done with one image")
     }
   }
 }
+
+##############################################
+######### Combined plots #####################
+##############################################
+setup_loop <- unique(CI$setup)
+n_loop <- unique(CI$n)
+dim_loop <- unique(CI$dim)
+
+CI_selected_sampled <- CI %>% filter(setup == "RsparseT2weak", n %in% c(100, 1000, 10000))
+lower_boundary <- floor(min(CI_selected_sampled$tau))
+upper_boundary <- ceiling(max(CI_selected_sampled$tau))
+
+set.seed(4123)
+CI %>% filter(setup == "RsparseT2weak", n %in% c(100, 1000, 10000), dim %in% c(20, 100)) %>%
+  mutate(n = paste("n =", n), dim = paste("dim =", dim)) %>%
+  mutate(dim = factor(dim,
+                      levels = c("dim = 20", "dim = 50", "dim = 100" ))) %>%
+  group_by(dim, n) %>% sample_n(80) %>%
+  ggplot() +
+  geom_point(aes(x = tau, y = pred)) +
+  geom_errorbar(aes(
+    x = tau,
+    ymax = X95.,
+    ymin = X5.
+  )) +
+  geom_abline(slope = 1) +
+  coord_cartesian(
+    xlim = c(-18, 18),
+    ylim = c(-18, 18)
+  ) +
+  theme_bw() +
+  ylab("estimated CATE") +
+  xlab("true CATE") +
+  facet_grid(dim~n) +
+  ggtitle(expression(paste(Y == W, "(", 4 * X[3] - 2 * X[1], ")",  + 3 * X[1] + 5* X[2]  + epsilon,
+                           "     ", W , " ~ Bern(.5)")))
+
+ggsave(
+  file = "tests/ConfidenceIntervals/plots/X__Summary_RsparseT2weak.pdf",
+  width = 11,
+  height = 7
+)
+
+
+
+
