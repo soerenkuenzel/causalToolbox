5f2b6a74953c4f460822fa58e162e13bfffff603
Soeren Kuenzel <soeren.kuenzel@gmail.com>
2017-04-16 11:48:28 -0700

    Setting up for BRC

--- Tune/TLearner/Rand_TuneT.R
+++ Tune/TLearner/Rand_TuneT.R
@@ -2,32 +2,32 @@
 library(hte)
 
 # We start by loading the setup and
-# args = (commandArgs(TRUE))
-# print(args)
-# setup_i <- -as.numeric(args)
-# print(setup_i)
+args = (commandArgs(TRUE))
+print(args)
+setup_i <- -as.numeric(args)
+print(setup_i)
 
-# setup_grid <- c(
-#     "RespSparseTau1strong",
-#     "RsparseT2weak",
-#     "complexTau",
-#     "Conf1",
-#     "rare1",
-#     "STMpp",
-#     "Ufail",
-#     "Usual1",
-#     "Wager1",
-#     "Wager2",
-#     "Wager3"
-#   )
-# setup <- setup_grid[setup_i]
-# print(setup)
+setup_grid <- c(
+     "RespSparseTau1strong",
+     "RsparseT2weak",
+     "complexTau",
+     "Conf1",
+     "rare1",
+     "STMpp",
+     "Ufail",
+     "Usual1",
+     "Wager1",
+     "Wager2",
+     "Wager3"
+   )
+ setup <- setup_grid[setup_i]
+ print(setup)
 
-setup <- "RespSparseTau1strong"
+#setup <- "RespSparseTau1strong"
 
 set.seed(59906)
-nthread <- 32
-nsamples <- 1e5
+nthread <- 24
+nsamples <- 10000
 dim <- 20
 ntrain <- 10000
 ntest <- 10000
