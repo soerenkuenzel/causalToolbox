699851e1e3620d6e541c8de89a63e0f82362cce4
soerenkuenzel <srk@berkeley.edu>
2017-04-19 06:28:29 -0700

    Updating test that for XhRF

---
+++ tests/testthat/test-XhRF.R
@@ -0,0 +1,19 @@
+test_that("Tests that XhRF is working correctly", {
+  set.seed(1423614230)
+
+  feat <- iris[, -1]
+  tr <- rbinom(nrow(iris), 1, .5)
+  yobs <- iris[, 1]
+
+  expect_warning(
+  xl <- X_RF(
+    feat = feat,
+    tr = tr,
+    yobs = yobs,
+    nthread = 8
+  ),
+  "honestRF is used as adaptive random forest."
+)
+
+  expect_equal(EstimateCate(xl, feat)[1], 0.1298953446, tolerance=1e-7)
+})
