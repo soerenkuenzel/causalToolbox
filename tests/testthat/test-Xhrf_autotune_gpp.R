ee50aba8a7650e0c8055af496600de1af0fc88e1
soerenkuenzel <srk@berkeley.edu>
2017-04-25 13:44:03 -0700

    Writing an example of how to change undertake hood parameters of other packages

--- tests/testthat/test-Xhrf_autotune_gpp.R
+++ tests/testthat/test-Xhrf_autotune_gpp.R
@@ -5,62 +5,63 @@ test_that("Tests test-Xhrf_gpp", {
   tr <- rbinom(nrow(iris), 1, .5)
   yobs <- iris[, 1]
 
-#
-#    ntree = 100
-#    nthread = 0
-#    verbose = TRUE
-#    init_points = 5
-#    n_iter = 1
-#
-#    starting_settings <- list(
-#      "start_setting_1" = get_setting_strong(feat, ntree, nthread),
-#      "start_setting_2" = get_setting_weak(feat, ntree, nthread)
-#    )
-#    setup_eval <-
-#      check_setups(starting_settings, feat, tr, yobs, ntree,
-#                   nthread, verbose)
-#
-#    starting_point <-
-#      starting_settings[[which.min(setup_eval$comb)]]
-#
-#    mean(as.numeric(evaluate_setting(starting_point, feat, tr, yobs)[1,]))
-#
-#
-#    Test_Fun_generic(starting_point,
-#                     feat,
-#                     tr,
-#                     yobs,
-#                     mtry_first = 1.2,
-#                     mtry_second = 1.5,
-#                     nodesizeAvg_first = 3.2,
-#                     nodesizeAvg_second = 3.3,
-#                     nodesizeSpl_first = 3.4,
-#                     nodesizeSpl_second = 3.1)
-#
-#
-#
-#   get_upper_bounds_for_nodesize(starting_point)
-#
-#   starting_point_optimized <-
-#     GP_optimize_small(starting_point, feat, tr, yobs)
-#
+  #
+  #    ntree = 100
+  #    nthread = 0
+  #    verbose = TRUE
+  #    init_points = 5
+  #    n_iter = 1
+  #
+  #    starting_settings <- list(
+  #      "start_setting_1" = get_setting_strong(feat, ntree, nthread),
+  #      "start_setting_2" = get_setting_weak(feat, ntree, nthread)
+  #    )
+  #    setup_eval <-
+  #      check_setups(starting_settings, feat, tr, yobs, ntree,
+  #                   nthread, verbose)
+  #
+  #    starting_point <-
+  #      starting_settings[[which.min(setup_eval$comb)]]
+  #
+  #    mean(as.numeric(evaluate_setting(starting_point, feat, tr, yobs)[1,]))
+  #
+  #
+  #    Test_Fun_generic(starting_point,
+  #                     feat,
+  #                     tr,
+  #                     yobs,
+  #                     mtry_first = 1.2,
+  #                     mtry_second = 1.5,
+  #                     nodesizeAvg_first = 3.2,
+  #                     nodesizeAvg_second = 3.3,
+  #                     nodesizeSpl_first = 3.4,
+  #                     nodesizeSpl_second = 3.1)
+  #
+  #
+  #
+  #   get_upper_bounds_for_nodesize(starting_point)
+  #
+  #   starting_point_optimized <-
+  #     GP_optimize_small(starting_point, feat, tr, yobs)
+  #
 
 
   capture.output(
     expect_warning(
-    xl_gpp <- X_RF_autotune_gpp(
-      feat,
-      tr,
-      yobs,
-      ntree = 100,
-      nthread = 0,
-      verbose = FALSE,
-      init_points = 5,
-      n_iter = 1),
-    "honestRF is used as adaptive random forest."
+      xl_gpp <- X_RF_autotune_gpp(
+        feat,
+        tr,
+        yobs,
+        ntree = 100,
+        nthread = 0,
+        verbose = FALSE,
+        init_points = 5,
+        n_iter = 1
+      ),
+      "honestRF is used as adaptive random forest."
     )
     ,
-    file='NUL'
+    file = 'NUL'
   )
 
   expect_equal(EstimateCate(xl_gpp, feat)[4],
@@ -74,4 +75,39 @@ test_that("Tests test-Xhrf_gpp", {
   expect_equal(CI[2, 3],
                0.1555463,
                tolerance = 1e-5)
+
+  ## Example for changing internal parameters of GPfit::GP_fit and
+  ## rBayesianOptimization::BayesianOptimization
+  # # faster speed:
+  # library(hte)
+  # feat <- iris[, -1]
+  # tr <- rbinom(nrow(iris), 1, .5)
+  # yobs <- iris[, 1]
+  #
+  # # slow, using good optimization:
+  # xl_gpp <- X_RF_autotune_gpp(
+  #   feat,
+  #   tr,
+  #   yobs,
+  #   ntree = 100,
+  #   nthread = 0,
+  #   verbose = TRUE,
+  #   init_points = 5,
+  #   n_iter = 1
+  # )
+  #
+  # # fast, but with worse optimzation:
+  # xl_gpp <- X_RF_autotune_gpp(
+  #   feat,
+  #   tr,
+  #   yobs,
+  #   ntree = 100,
+  #   nthread = 0,
+  #   verbose = TRUE,
+  #   init_points = 5,
+  #   n_iter = 1,
+  #   maxit = 2
+  # )
+
+
 })
