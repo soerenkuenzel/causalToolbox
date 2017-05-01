e080c23e7c246e7024409a81ff0c283087f1d6b1
soerenkuenzel <srk@berkeley.edu>
2017-04-30 22:25:56 -0700

    X_RF_fully_specified implemented

--- R/Xhrf.R
+++ R/Xhrf.R
@@ -38,9 +38,7 @@ setClass(
     m_tau_0 = "honestRF",
     m_tau_1 = "honestRF",
     m_prop = "honestRF",
-    predmode = "character",
-    relevant_Variable_first = "numeric",
-    relevant_Variable_second = "numeric",
+    hyperparameter_list = "list",
     creator = "function"
   )
 )
@@ -196,84 +194,150 @@ X_RF <-
       mtry_second <- ncol(feat)
     }
 
+    ############################################################################
+    # Translate the settings to a feature list
+    ############################################################################
+    general_hyperpara <- list("predmode" = predmode,
+                              "nthread" = nthread)
+    first_stage_hyperpara <- list(
+      "relevant_Variable" = relevant_Variable_first,
+      "ntree" = ntree_first,
+      "replace" = replace_first,
+      "sample.fraction" = sample_fraction_first,
+      "mtry" = mtry_first,
+      "nodesizeSpl" = min_node_size_spl_first,
+      "nodesizeAvg" = min_node_size_ave_first,
+      "splitratio" = splitratio_first,
+      "middleSplit" = middleSplit_first
+    )
+    second_stage_hyperpara <- list(
+      "relevant_Variable" = relevant_Variable_second,
+      "ntree" = ntree_second,
+      "replace" = replace_second,
+      "sample.fraction" = sample_fraction_second,
+      "mtry" = mtry_second,
+      "nodesizeSpl" = min_node_size_spl_second,
+      "nodesizeAvg" = min_node_size_ave_second,
+      "splitratio" = splitratio_second,
+      "middleSplit" = middleSplit_second
+    )
+    prop_hyperpara <- list(
+      "relevant_Variable" = relevant_Variable_prop,
+      "ntree" = ntree_prop,
+      "replace" = replace_prop,
+      "sample.fraction" = sample_fraction_prop,
+      "mtry" = mtry_prop,
+      "nodesizeSpl" = min_node_size_spl_prop,
+      "nodesizeAvg" = min_node_size_ave_prop,
+      "splitratio" = splitratio_prop,
+      "middleSplit" = middleSplit_prop
+    )
+
+    hyperparameter_list <- list(
+      "general" = general_hyperpara,
+      "l_first_0" = first_stage_hyperpara,
+      "l_first_1" = first_stage_hyperpara,
+      "l_second_0" = second_stage_hyperpara,
+      "l_second_1" = second_stage_hyperpara,
+      "l_prop" = prop_hyperpara
+    )
+
+    return(X_RF_fully_specified(feat = feat,
+                                tr = tr,
+                                yobs = yobs,
+                                hyperparameter_list = hyperparameter_list,
+                                verbose = verbose))
+  }
+
+#' @title X_RF_most_basic Constructor
+#' @rdname X_RF_fully_specified
+#' @description This is the most basic X-learner with honest random forest
+#' constructor. It should not be called by the user, since the list of
+#' parameters is too big. Instead call the simpler version XhRF or one of the
+#' self tuning versions
+#' @param feat feature data.frame.
+#' @param tr treatment assignment 0 for control and 1 for treatment.
+#' @param yobs the observed outcome.
+#' @param hyperparameter_list A list of lists of hyper parameters
+#' @param verbose TRUE for detailed output FALSE for no output
+#' @return A `X_RF` object.
+#' @export X_RF_fully_specified
+X_RF_fully_specified <-
+  function(feat,
+           tr,
+           yobs,
+           hyperparameter_list,
+           verbose) {
     yobs_0 <- yobs[tr == 0]
     yobs_1 <- yobs[tr == 1]
 
-    X_0 <- feat[tr == 0, ]
-    X_1 <- feat[tr == 1, ]
+    X_0 <- feat[tr == 0,]
+    X_1 <- feat[tr == 1,]
 
     m_0 <-
       honestRF(
-        x = X_0[, relevant_Variable_first],
+        x = X_0[ , hyperparameter_list[["l_first_0"]]$relevant_Variable],
         y = yobs_0,
-        ntree = ntree_first,
-        replace = replace_first,
-        # sampsize = ceiling(sample_fraction_first * length(yobs_0)),
-        sample.fraction = sample_fraction_first,
-        mtry = mtry_first,
-        nodesizeSpl = min_node_size_spl_first,
-        nthread = nthread,
-        splitrule =  'variance',
-        splitratio = splitratio_first,
-        nodesizeAvg = min_node_size_ave_first,
-        middleSplit = middleSplit_first
+        ntree = hyperparameter_list[["l_first_0"]]$ntree,
+        replace = hyperparameter_list[["l_first_0"]]$replace,
+        sample.fraction = hyperparameter_list[["l_first_0"]]$sample.fraction,
+        mtry = hyperparameter_list[["l_first_0"]]$mtry,
+        nodesizeSpl = hyperparameter_list[["l_first_0"]]$nodesizeSpl,
+        nodesizeAvg = hyperparameter_list[["l_first_0"]]$nodesizeAvg,
+        nthread = hyperparameter_list[["general"]]$nthread,
+        splitrule = "variance",
+        splitratio = hyperparameter_list[["l_first_0"]]$splitratio
       )
 
     m_1 <-
       honestRF(
-        x = X_1[, relevant_Variable_first],
+        x = X_1[ , hyperparameter_list[["l_first_1"]]$relevant_Variable],
         y = yobs_1,
-        ntree = ntree_first,
-        replace = replace_first,
-        # sampsize = ceiling(sample_fraction_first * length(yobs_1)),
-        sample.fraction = sample_fraction_first,
-        mtry = mtry_first,
-        nodesizeSpl = min_node_size_spl_first,
-        nthread = nthread,
-        splitrule =  'variance',
-        splitratio = splitratio_first,
-        nodesizeAvg = min_node_size_ave_first,
-        middleSplit = middleSplit_first
+        ntree = hyperparameter_list[["l_first_1"]]$ntree,
+        replace = hyperparameter_list[["l_first_1"]]$replace,
+        sample.fraction = hyperparameter_list[["l_first_1"]]$sample.fraction,
+        mtry = hyperparameter_list[["l_first_1"]]$mtry,
+        nodesizeSpl = hyperparameter_list[["l_first_1"]]$nodesizeSpl,
+        nodesizeAvg = hyperparameter_list[["l_first_1"]]$nodesizeAvg,
+        nthread = hyperparameter_list[["general"]]$nthread,
+        splitrule = "variance",
+        splitratio = hyperparameter_list[["l_first_1"]]$splitratio
       )
 
     if (verbose) {
       print("Done with the first stage.")
     }
-    r_0 <- predict(m_1, X_0[, relevant_Variable_first]) - yobs_0
-    r_1 <- yobs_1 - predict(m_0, X_1[, relevant_Variable_first])
+    r_0 <- predict(m_1, X_0[, hyperparameter_list[["l_first_0"]]$relevant_Variable]) - yobs_0
+    r_1 <- yobs_1 - predict(m_0, X_1[, hyperparameter_list[["l_first_1"]]$relevant_Variable])
 
     m_tau_0 <-
       honestRF(
-        x = X_0[, relevant_Variable_second],
+        x = X_0[, hyperparameter_list[["l_second_0"]]$relevant_Variable],
         y = r_0,
-        ntree = ntree_second,
-        replace = replace_second,
-        # sampsize = ceiling(sample_fraction_second * length(r_0)),
-        sample.fraction = sample_fraction_second,
-        mtry = mtry_second,
-        nodesizeSpl = min_node_size_spl_second,
-        nthread = nthread,
-        splitrule =  'variance',
-        splitratio = splitratio_second,
-        nodesizeAvg = min_node_size_ave_second,
-        middleSplit = middleSplit_second
+        ntree = hyperparameter_list[["l_second_0"]]$ntree,
+        replace = hyperparameter_list[["l_second_0"]]$replace,
+        sample.fraction = hyperparameter_list[["l_second_0"]]$sample.fraction,
+        mtry = hyperparameter_list[["l_second_0"]]$mtry,
+        nodesizeSpl = hyperparameter_list[["l_second_0"]]$nodesizeSpl,
+        nodesizeAvg = hyperparameter_list[["l_second_0"]]$nodesizeAvg,
+        nthread = hyperparameter_list[["general"]]$nthread,
+        splitrule = "variance",
+        splitratio = hyperparameter_list[["l_second_0"]]$splitratio
       )
 
     m_tau_1 <-
       honestRF(
-        x = X_1[, relevant_Variable_second],
+        x = X_1[, hyperparameter_list[["l_second_1"]]$relevant_Variable],
         y = r_1,
-        ntree = ntree_second,
-        replace = replace_second,
-        # sampsize = ceiling(sample_fraction_second * length(r_1)),
-        sample.fraction = sample_fraction_second,
-        mtry = mtry_second,
-        nodesizeSpl = min_node_size_spl_second,
-        nthread = nthread,
-        splitrule =  'variance',
-        splitratio = splitratio_second,
-        nodesizeAvg = min_node_size_ave_second,
-        middleSplit = middleSplit_second
+        ntree = hyperparameter_list[["l_second_1"]]$ntree,
+        replace = hyperparameter_list[["l_second_1"]]$replace,
+        sample.fraction = hyperparameter_list[["l_second_1"]]$sample.fraction,
+        mtry = hyperparameter_list[["l_second_1"]]$mtry,
+        nodesizeSpl = hyperparameter_list[["l_second_1"]]$nodesizeSpl,
+        nodesizeAvg = hyperparameter_list[["l_second_1"]]$nodesizeAvg,
+        nthread = hyperparameter_list[["general"]]$nthread,
+        splitrule = "variance",
+        splitratio = hyperparameter_list[["l_second_1"]]$splitratio
       )
     if (verbose) {
       print("Done with the second stage.")
@@ -281,19 +345,18 @@ X_RF <-
 
     m_prop <-
       honestRF(
-        x = feat[ , relevant_Variable_prop],
+        x = feat[, hyperparameter_list[["l_prop"]]$relevant_Variable],
         y = tr,
-        mtry = mtry_prop,
-        nodesizeSpl = min_node_size_spl_prop,
-        nodesizeAvg = min_node_size_ave_prop,
-        splitratio = splitratio_prop,
-        replace = replace_prop,
-        # sampsize = ceiling(sample_fraction_prop * length(tr)),
-        sample.fraction = sample_fraction_prop,
-        ntree = ntree_prop,
-        nthread = nthread,
-        middleSplit = middleSplit_prop
-        )
+        ntree = hyperparameter_list[["l_prop"]]$ntree,
+        replace = hyperparameter_list[["l_prop"]]$replace,
+        sample.fraction = hyperparameter_list[["l_prop"]]$sample.fraction,
+        mtry = hyperparameter_list[["l_prop"]]$mtry,
+        nodesizeSpl = hyperparameter_list[["l_prop"]]$nodesizeSpl,
+        nodesizeAvg = hyperparameter_list[["l_prop"]]$nodesizeAvg,
+        nthread = hyperparameter_list[["general"]]$nthread,
+        splitrule = "variance",
+        splitratio = hyperparameter_list[["l_prop"]]$splitratio
+      )
     if (verbose) {
       print("Done with the propensity score estimation.")
     }
@@ -308,34 +371,13 @@ X_RF <-
         m_tau_0 = m_tau_0,
         m_tau_1 = m_tau_1,
         m_prop = m_prop,
-        predmode = predmode,
-        relevant_Variable_first = relevant_Variable_first,
-        relevant_Variable_second = relevant_Variable_second,
+        hyperparameter_list = hyperparameter_list,
         creator = function(feat, tr, yobs) {
-          X_RF(
-            feat,
-            tr,
-            yobs,
-            predmode = predmode,
-            relevant_Variable_first = relevant_Variable_first,
-            relevant_Variable_second = relevant_Variable_second,
-            ntree_first = ntree_first,
-            ntree_second = ntree_second,
-            mtry_first = mtry_first,
-            mtry_second = mtry_second,
-            min_node_size_spl_first = min_node_size_spl_first,
-            min_node_size_ave_first = min_node_size_ave_first,
-            min_node_size_spl_second = min_node_size_spl_second,
-            min_node_size_ave_second = min_node_size_ave_second,
-            splitratio_first = splitratio_first,
-            splitratio_second = splitratio_second,
-            replace_first = replace_first,
-            replace_second = replace_second,
-            sample_fraction_first = sample_fraction_first,
-            sample_fraction_second = sample_fraction_second,
-            nthread = nthread,
-            verbose = FALSE
-          )
+          X_RF_fully_specified(feat,
+               tr,
+               yobs,
+               hyperparameter_list,
+               verbose)
         }
       )
     )
@@ -345,16 +387,6 @@ X_RF <-
 ############################
 ### Estimate CATE Method ###
 ############################
-# This is documented out, because we use the include statement in the beginning
-# of the document to be able to load the generic in many different places
-
-# setGeneric(name="EstimateCate",
-#            def=function(theObject, feature_new)
-#            {
-#              standardGeneric("EstimateCate")
-#            }
-# )
-
 #' EstimateCate-X_hRF
 #' @name EstimateCate-X_hRF
 #' @rdname EstimateCate-X_hRF
@@ -371,24 +403,25 @@ setMethod(
   {
     feature_new <- as.data.frame(feature_new)
 
+    predmode <- theObject@hyperparameter_list[["general"]]$predmode
     prop_scores <- predict(theObject@m_prop, feature_new)
-    if (theObject@predmode == "propmean") {
+    if (predmode == "propmean") {
       return(
         prop_scores        * predict(theObject@m_tau_0, feature_new) +
           (1 - prop_scores)  * predict(theObject@m_tau_1, feature_new)
       )
     }
-    if (theObject@predmode == "extreme") {
+    if (predmode == "extreme") {
       return(ifelse(
         prop_scores > .5,
         predict(theObject@m_tau_0, feature_new),
         predict(theObject@m_tau_1, feature_new)
       ))
     }
-    if (theObject@predmode == "control") {
+    if (predmode == "control") {
       return(predict(theObject@m_tau_0, feature_new))
     }
-    if (theObject@predmode == "treated") {
+    if (predmode == "treated") {
       return(predict(theObject@m_tau_1, feature_new))
     }
   }
