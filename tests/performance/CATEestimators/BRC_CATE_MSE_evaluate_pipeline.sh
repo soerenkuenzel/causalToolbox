91dc4f7124818a98f1fc04c4142d74b96840a841
Soeren Kuenzel <soeren.kuenzel@gmail.com>
2017-05-01 21:26:56 -0700

    Setting things up for BRC

--- tests/performance/CATEestimators/BRC_CATE_MSE_evaluate_pipeline.sh
+++ tests/performance/CATEestimators/BRC_CATE_MSE_evaluate_pipeline.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Job name:
-#SBATCH --job-name=XTS_all
+#SBATCH --job-name=CF
 #
 # Account:
-#SBATCH --account=co_praxis
+#SBATCH --account=fc_praxis
 #
 # Partition:
-#SBATCH --partition=savio
+#SBATCH --partition=savio2
 #
 # Request one node:
 #SBATCH --nodes=1
@@ -15,12 +15,12 @@
 #SBATCH --ntasks-per-node=1
 #
 # Processors per task:
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=24
 #
 # Wall clock limit:
 #SBATCH --time=72:00:00
 #
 ## Command(s) to run (example):#!/bin/bash
-R CMD BATCH --no-save --args -$1 CATE_MSE_evaluate_pipeline.R CATE_MSE_evaluate_setup_$1.Rout
+R CMD BATCH --no-save --args -$1 CATE_MSE_evaluate_pipeline.R CATE_MSE_CF_setup_$1.Rout
 
 
