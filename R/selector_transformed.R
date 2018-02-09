570b1d0762ebf15027a7576a565db73764b8b480
Ling Xie <lingxie@tanghaiyandeMacBook-Pro.local>
2018-02-09 03:26:19 -0800

    Added MSE and SD

--- R/selector_transformed.R
+++ R/selector_transformed.R
@@ -36,6 +36,7 @@ setMethod(
     
     # Calculate propensity score by using ranger
     temp_df <- data.frame(feat_train, tr_train)
+    # Convert integer labels to factors (required in ranger function?)
     temp_df$tr_train <- as.factor(temp_df$tr_train)
     rf <- ranger(tr_train ~ ., data = temp_df, probability = TRUE)
     propensity_score <- predict(rf, feat_test)$predictions[ ,2]
@@ -43,13 +44,18 @@ setMethod(
     # Calculate y_star_te 
     y_star_te <- yobs_test / (tr_test * propensity_score - 
                                 (1 - tr_test) * (1 - propensity_score))
+
+ 
     # Estimate CATE with the given learner function
     est <- estimator(feat = feat_test, tr = tr_test, yobs = yobs_test)
     cate <- EstimateCate(est, feat_test)
     
     # Calcualte the Goodness-of-Fit 
     gof <- - sum((y_star_te - cate)^2) / nrow(feat_test)
-    return(gof)
+    mse <- mean((y_star_te - cate)^2)
+    sd_err <- sd((y_star_te - cate)^2) / sqrt(nrow(feat_test))
+    
+    return(c(mse, sd_err))
   }
 )
 # Example
