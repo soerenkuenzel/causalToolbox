0ba97e8cd71773429834a180c1cf1cb2d7e76bdb
Soeren Kuenzel <soeren.kuenzel@gmail.com>
2018-02-09 16:30:21 -0800

    Improving variable names and potentially fixing a bug

--- R/selector_transformed.R
+++ R/selector_transformed.R
@@ -13,7 +13,7 @@ setGeneric(
 )
 #' GoF_yStar
 #' @name GoF_yStar
-#' @param feat a data frame of features 
+#' @param feat a data frame of features
 #' @param obs a vector of observations
 #' @param tr a vector of group assignment (assume entries are integers)
 #' @param estimator a learner constructor
@@ -23,37 +23,40 @@ setMethod(
   "GoF_yStar",
   definition = function(feat, yobs, tr, estimator, seed) {
     set.seed(seed)
-    # Split the given data frame into train/test data set. 
-    sample_idx <- sample.int(n = nrow(feat), size = floor(0.8 * nrow(feat)), 
-                             replace = FALSE)
-    feat_train <- feat[sample_idx, ]
+    # Split the given data frame into train/test data set.
+    sample_idx <-
+      sample.int(n = nrow(feat),
+                 size = floor(0.8 * nrow(feat)),
+                 replace = FALSE)
+    feat_train <- feat[sample_idx,]
     tr_train <- tr[sample_idx]
     yobs_train <- yobs[sample_idx]
     
-    feat_test <- feat[-sample_idx, ]
+    feat_test <- feat[-sample_idx,]
     tr_test <- tr[-sample_idx]
     yobs_test <- yobs[-sample_idx]
     
     # Calculate propensity score by using ranger
-    temp_df <- data.frame(feat_train, tr_train)
+    pscoreDS <- data.frame(feat_test, tr_test)
     # Convert integer labels to factors (required in ranger function?)
-    temp_df$tr_train <- as.factor(temp_df$tr_train)
-    rf <- ranger(tr_train ~ ., data = temp_df, probability = TRUE)
-    propensity_score <- predict(rf, feat_test)$predictions[ ,2]
+    pscoreDS$feat_test <- as.factor(pscoreDS$feat_test)
+    rf <- ranger::ranger(feat_test ~ ., data = pscoreDS, probability = TRUE)
+    propensity_score <- rf$predictions
     
-    # Calculate y_star_te 
-    y_star_te <- yobs_test / (tr_test * propensity_score - 
-                                (1 - tr_test) * (1 - propensity_score))
-
- 
+    # Calculate y_star_te
+    y_star_te <- yobs_test / 
+      (tr_test * propensity_score - (1 - tr_test) * (1 - propensity_score))
+    
+    
     # Estimate CATE with the given learner function
-    est <- estimator(feat = feat_test, tr = tr_test, yobs = yobs_test)
-    cate <- EstimateCate(est, feat_test)
+    estimator_trained <- estimator(feat = feat_train, 
+                                   tr = tr_train, 
+                                   yobs = yobs_train)
+    cate <- EstimateCate(estimator_trained, feat_test)
     
-    # Calcualte the Goodness-of-Fit 
-    gof <- - sum((y_star_te - cate)^2) / nrow(feat_test)
-    mse <- mean((y_star_te - cate)^2)
-    sd_err <- sd((y_star_te - cate)^2) / sqrt(nrow(feat_test))
+    # Calcualte the Goodness-of-Fit
+    mse <- mean((y_star_te - cate) ^ 2)
+    sd_err <- sd((y_star_te - cate) ^ 2) / sqrt(nrow(feat_test))
     
     return(c(mse, sd_err))
   }
